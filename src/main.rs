3c89b95fe7111cd03b4a11244b1b36ae8c878a35
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2022-09-21 18:44:03 +0200

    basic execution of commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
+use std::{io::{stdin, stdout, Write}, process::Command};
 
-    println!("hell world")
+fn main(){
+    loop {
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush();
 
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command)
+            .spawn()
+            .unwrap();
+
+        // don't accept another command until this one completes
+        child.wait(); 
+    }
 }
