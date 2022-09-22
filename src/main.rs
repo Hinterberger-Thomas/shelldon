99b3faf2ea19d8ca60e0b7ffb91794cbe9e58811
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2022-09-22 19:23:26 +0200

    base error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::{io::{stdin, stdout, Write}, process::Command};
+use std::{
+    io::{stdin, stdout, Write},
+    process::Command,
+};
 
-fn main(){
+fn main() {
+    let current_directory = Command::new("pwd").arg("").output().expect("msg");
     loop {
         // use the `>` character as the prompt
         // need to explicitly flush this to ensure it prints before read_line
-        print!("> ");
+        print!(
+            "{} -> ",
+            String::from_utf8_lossy(&current_directory.stdout).trim()
+        );
         stdout().flush();
 
         let mut input = String::new();
@@ -12,11 +19,14 @@ fn main(){
 
         let command = input.trim();
 
-        let mut child = Command::new(command)
-            .spawn()
-            .unwrap();
+        let child = Command::new(command).spawn();
 
-        // don't accept another command until this one completes
-        child.wait(); 
+        let error_message = match child {
+            Ok(mut child) => {
+                child.wait(); "".to_string()
+            }
+            Err(ve) => ve.to_string(),
+        };
+        println!("{}",error_message)
     }
 }
